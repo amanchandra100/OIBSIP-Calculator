f259d8876b4ab96cdf3da5a1471aaddb2179f3bd
Aman Chandra <130493204+amanchandra10@users.noreply.github.com>
2024-03-12 21:25:34 +0530

    Update index.js

--- index.js
+++ index.js
@@ -25,11 +25,11 @@ btn.addEventListener('click',(btn1)=>{
     const cssfile=document.getElementById("cssfile");
     const btn12=document.getElementById("btndark")
     if (btn12.innerHTML == 'Dark mode') {
-        cssfile.setAttribute('href','./dark_mode.css');
+        cssfile.setAttribute('href','./style1.css');
         btn12.innerHTML="Light mode";
     }
      else {
-        cssfile.setAttribute('href','./light_mode.css');
+        cssfile.setAttribute('href','./style.css');
         btn12.innerHTML="Dark mode";
     }  
 
